42e822a10652aced62973821ef19d101c2dbdbda
ii-codeLab <avalosvana@gmail.com>
2022-02-28 14:23:07 -0600

    función para personajes en clase people

--- src/data.js
+++ src/data.js
@@ -5,16 +5,15 @@ export default function generatorHtml(card){
   <h3>${card.rt_score}</h3></section>`
 }
  
-/*export function characterGenerator (character){
+export function characterGenerator (character){
   return `<section class="item"><img src="${character.img}">
   <h2>${character.name}(${character.gender})</h2>
   <h3>${character.specie}</h3></section>`
-}*/
+}
 
 export function searchFilters(input,selector){
  document.addEventListener("keyup",(e) =>{
    if(e.target.matches(input)){
-     //console.log(e.key);
      console.log(e.target.value);
      document.querySelectorAll(selector).forEach((el) => 
       el.textContent.toLowerCase().includes(e.target.value)
