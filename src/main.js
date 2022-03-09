120e7aa5d4283eab3f0e3fd8d09a56bfd421c32a
ii-codeLab <avalosvana@gmail.com>
2022-03-08 22:06:35 -0600

    Calcifers y filtrado productor/director

--- src/main.js
+++ src/main.js
@@ -9,14 +9,17 @@ import generatorHtml from './data.js';
 import {characterGenerator} from './data.js';
 import { sortData } from './data.js';
 import{filterFilms} from './data.js';
-
+import { filterDirector } from './data.js';
 
 //---------------VARIABLES----------------------------------------------------------
-//----------llamando input search------------
+//----------llamando input search------------------
 const search=document.querySelector(".search");
 
-// Variables para ordenar
+//----------Variables para ordenar-----------------
 const nodoMenuO = document.querySelector(".menuO");
+
+//-----------Variables para filtrar----------------
+const nodoMenuF = document.querySelector(".menuF");
 
 //----------------------Variables para mostrar películas----------------------------
 const nodoRoot = document.querySelector(".root");
@@ -25,16 +28,17 @@ let todoHtml=[];      //cambiamos de string a array
 //----------------------Variables para mostrar personajes----------------------------
 const nodoPeople=document.querySelector(".people");
 const movies= data.films 
+//let currentMovies = "";
 const allCharacters=[];
-let todoCharacters=""
-
-//-----------------------INTERACCIÓN-------------------------------------------------
+let todoCharacters="";
+//-----------------------*INTERACCIÓN*-------------------------------------------------
 
 //------------------------PELÍCULAS--------------------------------------------------
 //------------Va almacenando las tarjetas de cada película---------------------------
 movies.forEach(card=>{
   todoHtml += generatorHtml(card) 
 })
+//console.log(todoHtml);
 //------------Muestra las tarjetas de las películas en HTML---------------------------
 nodoRoot.innerHTML= todoHtml;
 
@@ -43,6 +47,13 @@ nodoRoot.innerHTML= todoHtml;
 for (let index = 0; index < movies.length; index++) {
    allCharacters.push(movies[index].people);
 } 
+//console.log(allCharacters);
+
+/*let calcifers = "";
+for (let i= 0; i < movies.length; i++) {
+  calcifers.push(movies[i].rt_score);
+} 
+console.log(calcifers);*/
 
 /* ---------Primero accede al arreglo de personajes de cada peícula y después aaccede a 
 cada uno de los personajes por película----------------------------------------------*/
@@ -51,21 +62,30 @@ allCharacters.forEach(character=>{
     todoCharacters += characterGenerator(element)})
 })
 
-
-//------------------------------Filtrado----------------------------------------------
+//------------------------------Buscador----------------------------------------------
 search.addEventListener("keyup",(e) => {
-  let searchFilm=filterFilms(e.target.value.toLowerCase(),movies);
-  let htmlSearch=""
+  let searchFilm = filterFilms(e.target.value.toLowerCase(),movies);
+  let htmlSearch = ""
   searchFilm.forEach(everyFilm =>{
-      htmlSearch+=generatorHtml(everyFilm)
-     
+      htmlSearch += generatorHtml(everyFilm)      
   })
- nodoRoot.innerHTML=htmlSearch;
+ nodoRoot.innerHTML = htmlSearch;
   })
+
+ //------------------------------Filtrado por director--------------------------------
+ nodoMenuF.addEventListener("change",(e)=>{
+  let filterD = filterDirector(e.target.value,movies)
+  console.log(e.target.value);
+  let directors = "";
+  filterD.forEach(oneFilm =>{
+    directors += generatorHtml(oneFilm);
+  })
+  nodoRoot.innerHTML = directors;
+})
 
 //-------------------------------Ordenado---------------------------------------------
 nodoMenuO.addEventListener("change",(e)=>{
-  let sortFilms = sortData(e.target.value,movies);
+  let sortFilms = sortData(e.target.value, movies);
   let htmlSort = ""
   sortFilms.forEach(oneFilm =>{
     htmlSort += generatorHtml(oneFilm)
